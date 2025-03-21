3770e8b70bf4e6dc0025361e081c2d9365d00921
Exequiel <exequiel.miranda245@gmail.com>
2025-03-21 12:08:08 -0600

    asdf

--- backend/database.js
+++ backend/database.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
-
-// 1- Configuro la URI o dirección de la base de datos
-const URI = "mongodb://localhost:27017/FerreteriaEPADB20170508";
+import { config } from "./src/config.js";
 
 // 2- Conecto la base de datos
-mongoose.connect(URI);
+mongoose.connect(config.db.URI);
 
 // ------ Comprobar que todo funciona ------
 
